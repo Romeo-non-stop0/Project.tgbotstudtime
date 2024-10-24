6cccab124f912164eba468cfb47afe378b21fc4c
Roman Khryashchev <khryashchev@gmail.com>
2024-10-24 18:40:13 +0300

    notification+schedule

--- codebot.py
+++ codebot.py
@@ -3,10 +3,12 @@ import telebot
 import datetime
 import time
 from telebot import types
+from notifiers import get_notifier, notify
+import threading
 
-# Токен бота, который выдается в процессе создания бота Telegram, вставляем вместо 'ваш токен'
 bot = telebot.TeleBot('7830171145:AAEOU0N9FDRUbehoUpo_4GZYFsTPuFRKtlc')
-# Обработка команды /start
+
+notifications = {}
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, "Привет, я бот с расписанием занятий! Выберите действие:", reply_markup=keyboard)
@@ -16,9 +18,8 @@ keyboard = types.ReplyKeyboardMarkup(row_width=1)
 button1 = types.KeyboardButton("Расписание")
 button2 = types.KeyboardButton("Дедлайны")
 button3 = types.KeyboardButton("Каникулы и сессии")
-keyboard.add(button1, button2, button3)
-
-
+button4 = types.KeyboardButton("Включить уведомления на дедлайны")
+keyboard.add(button1, button2, button3, button4)
 
 # Обработка нажатий на кнопки
 @bot.message_handler(func=lambda message: True)
@@ -29,14 +30,11 @@ def handle_buttons(message):
         bot.reply_to(message, "тут пока ничего нет(")
     elif message.text == "Каникулы и сессии":
         bot.reply_to(message, "тут пока ничего нет(")
+    elif message.text == "Включить уведомления на дедлайны":
+        handle_notifications(message)
 
 # Список с расписанием занятий, Чётная = Schedule 2, Нечётная  Schedule 1
 schedule1 = {
-    # 'Monday': [
-        # {'Время': '13:50', 'Предмет': 'Л-309, Автоматизация систем электроснабжения(лек)'}, # "перое знач.":"второе знач." - меняем только второе значение, под свои задачи.
-        # {'Время': '15:40', 'Предмет': 'Л-301, Транспортная безопасность(пр)'},
-        # {'Время': '17:30', 'Предмет': 'Л-510, Телемеханизация в системе электроснабжения(доп)'}
-    # ],
     'Tuesday': [
         {'Время': '9:30 - 10:50', 'Предмет': 'БП-126, лекция, Дискретная математика (Мокеев Д.Б.'},
         {'Время': '11:10 - 12:30', 'Предмет': 'БП-405, семинар, Электроснабжение железных дорог (Цаплина Е.К.)'},
@@ -47,9 +45,6 @@ schedule1 = {
         {'Время': '11:10 - 12:30', 'Предмет': 'Л-323, лекция, Программирование С/С++ (Пеплин Ф.С.)'},
         {'Время': '13:00 - 14:20', 'Предмет': 'Л-225, лекция, Линейная алгебра и геометрия (Савина О.Н.)'},
         {'Время': '14:40 - 16:00', 'Предмет': 'БП-406, семинар, Математический анализ (Чистяков В.В.)'},
-    # ],
-    # 'Thursday': [
-
     ],
     'Friday': [
         {'Время': '13:00 - 14:20', 'Предмет': 'online, семинар, Основы Российской Государственности (Константинова Т.Н.)'},
@@ -61,7 +56,6 @@ schedule1 = {
     ]
 }
 
- # Дни недели можно удалять или добавлять. Главное сохранить синтаксис списка, не потерять символы. Название дня недели должно быть на английском языке.
 schedule2 = {
     'Monday': [
         {'Время': '11:10 - 12:30', 'Предмет': 'БП-216, практика, Программирование на С++'},
@@ -77,9 +71,6 @@ schedule2 = {
         {'Время': '11:10 - 12:30', 'Предмет': 'Л-323, лекция, Программирование С/С++ (Пеплин Ф.С.)'},
         {'Время': '13:00 - 14:20', 'Предмет': 'Л-225, лекция, Линейная алгебра и геометрия (Савина О.Н.)'},
         {'Время': '14:40 - 16:00', 'Предмет': 'БП-406, семинар, Математический анализ (Чистяков В.В.)'},
-    # ],
-    # 'Thursday': [
-
     ],
     'Friday': [
         {'Время': '13:00 - 14:20', 'Предмет': 'online, семинар, Основы Российской Государственности (Константинова Т.Н.)'},
@@ -90,48 +81,57 @@ schedule2 = {
         {'Время': '9:30 - 10:50', 'Предмет': 'Л-325, Английский язык (Черницкая М.Б.)'}
     ]
 }
-
 now = datetime.datetime.now()        # Получаем текущую дату с помощью библиотеки datetime
 week_number = now.isocalendar()[1]   # Получаем номер недели в году с помощью библиотеки datetime
 
 # Функция для получения расписания на текущий день
 def get_today_schedule():
-    current_day = datetime.datetime.now().strftime("%A")  # current_day получает ответ в виде дня недели на английском языке с помощью библиотеки datetime
-    if week_number % 2 == 0 and current_day in schedule2: # Если условие (деление номера недели на 2 без остатка) верно и в списке есть день, который совпадает с текущим,
-        return schedule2[current_day]                     # функция вернёт ответ в виде списка Schedule 2
-    elif current_day in schedule1:                        # Если условие деления не верно, функция вернёт ответ в виде списка Schedule 1
+    current_day = datetime.datetime.now().strftime("%A")
+    if week_number % 2 == 0 and current_day in schedule2:
+        return schedule2[current_day]
+    elif current_day in schedule1:
         return schedule1[current_day]
-    else:                                                 # Если в списке нет текущего дня недели, функция вернёт пустой ответ
+    else:
         return []
 
 # Функция для форматированного вывода расписания в текстовом формате
 def format_schedule(schedule):
-    if not schedule:                                                     # Если функция get_today_schedule вернула пустой ответ,
-        return "Сегодня пар нет!"                                        # функиця format_schedule вернёт ответ "Сегодня пар нет!"
-    else:                                                                # Если функция get_today_schedule вернула ответ в виде одного из списков, format_schedule вернёт ответ в виде переменной result
-        result = "Расписание на сегодня:\n"                              # переменная result имеет вид string (по русски = строка), Символ новой строки в Python — это \n . Он используется для обозначения окончания строки текста
-        for lesson in schedule:                                          # for in - это цикл. Общий синтаксис for... in в python выглядит следующим образом: for <переменная> in <последовательность>:< действие> else:< действие>.
-            result += f"{lesson['Время']} - {lesson['Предмет']}\n"       # Элементы «последовательности» перебираются один за другим «переменной» цикла; если быть точным, переменная указывает на элементы.
-        return result                                                    # "+-" - это инкрементальные конкатенации в цикле. Тема не самая простая, об этом позже. f-строки позволяют форматировать информацию в нужном нам виде
+    if not schedule:
+        return "Сегодня пар нет!"
+    else:
+        result = "Расписание на сегодня:\n"
+        for lesson in schedule:
+            result += f"{lesson['Время']} - {lesson['Предмет']}\n"
+        return result
 
 
-# Функция для отправки расписания на текущий день в чат
-def send_today_schedule(chat_id):
-    bot.send_message(chat_id, format_schedule(get_today_schedule()))     # bot.send_message отправляет данные в чат с ботом
-
-# Задаем обработчик команды /schedule
-# @bot.message_handler(commands=['schedule'])
-# def send_schedule(message):
-#     send_today_schedule('1984839991')        # Вместо "айди чата с ботом" вставляем chatid своего чата
-
-# Определяем интервал для автоматической отправки расписания
-# interval = 60 * 60  # Отправлять расписание каждый час (измеряется в секундах, поэтому 60 сек * 60 = 1 час)
-# Запускаем цикл для автоматической отправки расписания
-# while True:
-#     send_today_schedule('1984839991')  # Замените <chat_id> на идентификатор нужного чата
-#     time.sleep(interval)                      # time.sleep запускает таймер ожидания с перменной interval
+#Уведомления
+def handle_notifications(message):
+    msg = bot.send_message(message.chat.id, "Введите количество минут до напоминания:")
+    bot.register_next_step_handler(msg, process_minutes)
+def process_minutes(message):
+    try:
+        minutes = int(message.text)
+        if minutes < 1:
+            raise ValueError("Количество минут должно быть положительным.")
+        notifications[message.chat.id] = {'minutes': minutes}
+        msg = bot.send_message(message.chat.id, "Введите название для дедлайна:")
+        bot.register_next_step_handler(msg, process_deadline, minutes)
+    except ValueError:
+        bot.send_message(message.chat.id, "Пожалуйста, введите корректное количество минут.")
+        msg = bot.send_message(message.chat.id, "Введите количество минут до напоминания:")
+        bot.register_next_step_handler(msg, process_minutes)
+def process_deadline(message, minutes):
+    deadline_name = message.text
+    notifications[message.chat.id]['name'] = deadline_name
+    # Устанавливаем время напоминания
+    bot.send_message(message.chat.id, f"Уведомление для '{deadline_name}' установлено на {minutes} минут.")
+    # Запускаем таймер для отправки уведомления
+    threading.Timer(minutes * 60, send_notification, args=(message.chat.id, deadline_name)).start()
+def send_notification(chat_id, deadline_name):
+    # Отправка уведомления пользователю
+    bot.send_message(chat_id, f'Не забудьте о дедлайне: {deadline_name}')
 
 bot.polling(none_stop=True, interval=0)   # Запускаем бота
+
 #В этом примере бот отвечает на команду /schedule отправкой расписания занятий на текущий день в чат.
-# Также цикл While отправляет расписание в заданный чат каждый заданный интервал времени.
-# Вы можете использовать этот пример как основу для своего Telegram-бота с расписанием занятий и автоматической отправкой.
